a3bc5c5af065d5cd648611adc2b1174ddb210c71
Annette0000 <55258543+Annette0000@users.noreply.github.com>
2020-04-29 04:43:27 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
-//const { createStore } = require('./utils');
+const { createStore } = require("./utils");
 const LaunchAPI = require("./datasources/launch");
-//const UserAPI = require('./datasources/user');
+const UserAPI = require("./datasources/user");
 
-//const store = createStore();
+const store = createStore();
 
 const resolvers = {
   Query: {
@@ -15,8 +15,8 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   dataSources: () => ({
-    launchAPI: new LaunchAPI()
-    // userAPI: new UserAPI({ store })
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
   }),
   resolvers
 });
