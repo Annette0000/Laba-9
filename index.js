54f3cd284b15be3e1ffe60e00a6e01b81e9d7c96
Annette0000 <55258543+Annette0000@users.noreply.github.com>
2020-04-29 04:49:48 +0300

    Resolves added

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const typeDefs = require("./schema");
 const { createStore } = require("./utils");
 const LaunchAPI = require("./datasources/launch");
 const UserAPI = require("./datasources/user");
+const resolvers = require("./resolvers");
 
 const store = createStore();
-
+/*
 const resolvers = {
   Query: {
     hell: () => "aaaaaa!"
   }
-};
+};*/
 
 const server = new ApolloServer({
   typeDefs,
